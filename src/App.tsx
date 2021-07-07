282ef7d0928ceb0ba4c4bd77ecd66e9e3c58513b
Gabriel Sanchez Arrisi <gsanchezarrisi@outlook.com>
2021-07-07 19:55:03 -0400

    implementacion de logica de tarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PerfilDesarrollador from './components/perfilDesarrollador/PerfilDesarrollador';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PerfilDesarrollador 
+        nombre="Gabriel Sánchez" 
+        edad={30}
+        tienePantalla={false}
+        />
     </div>
   );
 }
---
+++ src/components/perfilDesarrollador/PerfilDesarrollador.tsx
@@ -0,0 +1,23 @@
+type PerfilDesarrolladorProps = {
+    nombre: string,
+    edad: number,
+    tienePantalla: boolean,
+};
+
+const PerfilDesarrollador = (props: PerfilDesarrolladorProps) => {
+    
+    function convertTienePantalla(tienePantalla: boolean): string {
+        return (tienePantalla) ? "Si tiene pantalla" : "No tiene pantalla";
+    };
+
+    return(
+        <ol type="A">
+            <li>Mi nombre es: {props.nombre}</li>
+            <li>Mi edad es: {props.edad} Años</li>
+            <li>Estado Monitor: {convertTienePantalla(props.tienePantalla)}</li>
+        </ol>
+    );
+
+};
+
+export default PerfilDesarrollador;
